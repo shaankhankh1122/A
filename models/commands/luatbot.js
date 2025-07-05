a74fdd706b390352e5cb8078bf2cdae20ce21710
Uzairkhan12346 <tempmailuzair@gmail.com>
2025-07-05 23:50:31 +0500

    Update luatbot.js

--- models/commands/luatbot.js
+++ models/commands/luatbot.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "RuleBot",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+	credits: "uzairrajput", 
 	description: "no prefix",
 	commandCategory: "group",
 	usages: "RuleBot",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("RuleBot")==0 || (event.body.indexOf("RuleBot")==0)) {
 		var msg = {
-				body: "When Using Chat Bot Users Should Note Some Things Below\n❯ SourceCode ChatBot Made By Miraiteam & mod by P-SeverTeam\n❯ Users Avoid Spam Bot More than 20 Times/Day If Spam Bot 20 Times/Day Same ",
+				body: "When Using Chat Bot Users Should Note Some Things Below\n❯ SourceCode ChatBot Made By Uzair Rajput Mtx\n❯ Users Avoid Spam Bot More than 20 Times/Day If Spam Bot 20 Times/Day Same ",
 			}
 			api.sendMessage(msg, threadID, messageID);
 		}
