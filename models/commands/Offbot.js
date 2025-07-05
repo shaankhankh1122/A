5c7f05a64d07aa7f858f7e00ab921cef55592643
Uzairkhan12346 <tempmailuzair@gmail.com>
2025-07-05 23:38:24 +0500

    Update Offbot.js

--- models/commands/Offbot.js
+++ models/commands/Offbot.js
@@ -2,13 +2,13 @@ module.exports.config = {
 	name: "offbot",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "uzairrajput",
 	description: "turn the bot off",
 	commandCategory: "system",
 	cooldowns: 0
         };
 module.exports.run = ({event, api}) =>{
-    const permission = ["100016828397863", "100016828397863"];
-  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For only Shaan", event.threadID, event.messageID);
-  api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
+    const permission = ["61552682190483", "61552682190483"];
+  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For only Uzair Rajput", event.threadID, event.messageID);
+  api.sendMessage(`[ OK ] ${global.config.BOTNAME} Uzair Mtx Bot are now turned off.`,event.threadID, () =>process.exit(0))
 }
